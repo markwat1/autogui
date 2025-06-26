5985f071a6007d617d1b4241ee4b1e94daccbd4f
mark <mark@watana.be>
2025-06-27 06:31:22 +0900

    initial

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use rustautogui;
+
+fn main() {
+    let rag = rustautogui::RustAutoGui::new(false); // arg: debug
+    let mut rag = match rag {
+        Ok(rag) => {
+            rag
+        },
+        Err(e) => {
+            panic!("Error: {}", e);
+        }
+    };   
+    let screen_size = rag.get_screen_size();
+    println!("Screen size: {:?}", screen_size);
+    rag.prepare_template_from_file(
+       "close_button.png",
+       Some((0,0,screen_size.0.try_into().unwrap(),screen_size.1.try_into().unwrap())),
+       rustautogui::MatchMode::Segmented
+    ).unwrap();
+
+    let right_down = (screen_size.0 - 1, screen_size.1 - 1);
+    let mut pre_position = (0,0);
+    while rag.get_mouse_position().unwrap() != right_down {
+        let new_position = rag.get_mouse_position().unwrap();
+        if new_position != pre_position {
+            println!("Mouse position: {:?}", new_position);
+        }
+        pre_position = new_position;
+//        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+    let found = rag.find_image_on_screen(0.9);
+    println!("Found: {:?}", found);
+}
+
